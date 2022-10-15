e7c397265847936a0f6bb48eee83100624a49970
tayyab <tayyabnaveed5341@gmail.com>
2022-10-15 20:12:16 +0500

    response demo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\Recommendation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('consumption', function(Request $r){
+    return [
+        "upgrades"=>[
+            [
+                "name"=>"upg 1",
+                "estimated_saving"=>"23",
+                "link"=>""
+            ], [
+
+                "name"=>"upg 2",
+                "estimated_saving"=>"34",
+                "link"=>""
+            ]
+        ],
+        "investment"=>5678, // currency
+        "roi"=>9, // years
+        "saving"=>90, // % on bill
+        "bill_after"=>123 // currency
+    ];
+    //new Recommendation();
 });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
